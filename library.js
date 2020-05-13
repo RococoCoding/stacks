cdcfa357e191c5128c28ade9de45ff7d412ffe27
sinsofangels <63833718+sinsofangels@users.noreply.github.com>
2020-05-12 20:41:50 -0400

    +save read status to library

--- library.js
+++ library.js
@@ -1,56 +1,15 @@
-//*to do later -- support for multiple authors? series?
-let library = [
-    {
-        name: "The Color of Magic",
-        authorFirst: "Terry",
-        authorLast: "Pratchett",
-        pages: 228,
-        year: 2005,
-        read: "yes",
-    },
-    {
-        name: "The Story of the Stone",
-        authorFirst: "Barry",
-        authorLast: "Hughart",
-        pages: 289,
-        year: 1989,
-        read: "yes",
-    },
-    {
-        name: "The Hobbit",
-        authorFirst: "JRR",
-        authorLast: "Tolkien",
-        pages: 310,
-        year: 1937,
-        read: "yes",
-    },
-    {
-        name: "Cryptonomicon",
-        authorFirst: "Neal",
-        authorLast: "Stephenson",
-        pages: 1129,
-        year: 2002,
-        read: "yes",
-    },{
-        name: "The Graveyard Book",
-        authorFirst: "Neil",
-        authorLast: "Gaiman",
-        pages: 307,
-        year: 2008,
-        read: "yes",
-    },
-
-];
-
-function bookMaker(name, authorFirst, authorLast, pages, year, read) {
-    this.name = name;
-    this.authorFirst = authorFirst;
-    this.authorLast = authorLast;
-    this.pages = pages;
-    this.year = year;
-    this.read = read;
+function outer() {
+    let counter = 0;
+    return function inner() {
+        counter++;
+        return counter;
+    }
 }
+const callOuter = outer();
+callOuter();
+callOuter();
+callOuter();
 
-function addBookToLibrary(book) {
-    library.push(book);
-}
+const callOuterToo = outer();
+callOuterToo();
+console.log(callOuterToo());
